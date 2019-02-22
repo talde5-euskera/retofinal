f7853d6504c530d2a26bed3d5bc0f61be683c8bb
talde5-euskera <45936166+talde5-euskera@users.noreply.github.com>
2019-02-22 13:00:19 +0100

    cambios 22/02

--- reto-master/resources/views/Alumno/index.blade.php
+++ reto-master/resources/views/Alumno/index.blade.php
@@ -6,7 +6,16 @@
   if(exist){
     alert(msg);
   }
+  $(document).ready(function(){
+    $("#myInput").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#filtro h4").filter(function() {
+          $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+      });
+    });
 </script>
+
 <!-- End of Header Area -->
 <!--Start of Candidates Area-->
     <div class="candidates-area ptb-120">
@@ -50,13 +59,17 @@
             </div>
             <div class="row">
                 <div class="col-md-12">
+                    <div class="text-center mb-35">
+                        <input id="myInput" type="text" placeholder="Buscar..">
+                        <a href="{{ url('/alumno/create')}}" class="button large-button">Añadir Alumno</a>
+                    </div>
                     <div class="job-post-container fix mb-70">
 
              
                 @foreach($alumnos as $alumno) 
 
 
-                        <div class="single-job-post fix">
+                        <div id="filtro" class="single-job-post fix">
                             <div class="job-title col-3 pl-30">
                                 
                                 <span class="pull-left block mtb-17">
@@ -76,21 +89,21 @@
                             </div>
                             <div class="keyword col-4 pl-20 pt-39">
                                 <a href="{{ url('/alumno/show/'.$alumno->id )}}" class="button mr-10">&#9997; Detalles</a>
-                                @if($alumno->baja == 0)
-                                <a href="{{url('/alumno/desactivar/'.$alumno->id)}}" class="button mr-10">Desactivar</a>
-                                @else
-                                <a href="{{url('/alumno/activar/'.$alumno->id)}}" class="button mr-10">
-                                Activar</a>
+                                @if(session('admin') == "si")
+
+                                    @if($alumno->baja == 0)
+                                    <a href="{{url('/alumno/desactivar/'.$alumno->id)}}" class="button mr-10">Desactivar</a>
+                                    @else
+                                    <a href="{{url('/alumno/activar/'.$alumno->id)}}" class="button mr-10">
+                                    Activar</a>
+                                    @endif
+                                    <a href="{{url('/alumno/eliminar/'.$alumno->id)}}" class="button">&#x2717; Eliminar</a>
                                 @endif
-                                <a href="{{url('/alumno/eliminar/'.$alumno->id)}}" class="button">&#x2717; Eliminar</a>
                             </div>
                         </div>
               @endforeach
 
                         </div>
-                    </div>
-                    <div class="text-center">
-                        <a href="{{ url('/alumno/create')}}" class="button large-button">Añadir Alumno</a>
                     </div>
                 </div>
             </div>
